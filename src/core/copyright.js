4d2386e28117e88cd06aeccaf36e2bafb0e893bd
neil dingeldein <ndingeldein@gmail.com>
2025-06-30 11:36:28 -0500

    release v0.1.4

--- src/core/copyright.js
+++ src/core/copyright.js
@@ -14,6 +14,10 @@ export default class Copyright {
     this.selector = selector
     this.settings = mergeOptions(defaults, options)
     this.container = document.querySelector(this.selector)
+    // get domain name from window.location.href
+    this.domainName = window.location.href.split('/')[2]
+
+    this.logoLinkUrl = `https://www.modiphy.com?utm_campaign=copyright_footer&utm_content=${this.domainName}`
 
     this.currentYear = new Date().getFullYear()
     this.companyName = this.settings.companyName
@@ -29,7 +33,7 @@ export default class Copyright {
       <span>All rights reserved</span>
       ${addPipe(this.settings.pipeColor, this.settings.pipeOpacity, this.settings.pipeMargin)}
       <span>Built by</span>
-      <a id="modiphy-copyright-logo" href="https://www.modiphy.com" title="MODIPHY® WEB DESIGN" style="color: inherit;"><span style="position: absolute;width: 1px;height: 1px;padding: 0;margin: -1px;overflow: hidden;clip: rect(0, 0, 0, 0);white-space: nowrap;border-width: 0;">MODIPHY® WEB DESIGN</span>${modiphyLogo(
+      <a id="modiphy-copyright-logo" href="${this.logoLinkUrl}" title="MODIPHY® WEB DESIGN" style="color: inherit;"><span style="position: absolute;width: 1px;height: 1px;padding: 0;margin: -1px;overflow: hidden;clip: rect(0, 0, 0, 0);white-space: nowrap;border-width: 0;">MODIPHY® WEB DESIGN</span>${modiphyLogo(
         this.settings.logoColor,
         this.settings.logoHeight,
         this.settings.logoMargin,
